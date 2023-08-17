b64e176ffff3aa9e26a19d2fdde70abbcdc51791
ViktoriiaMatviichuk <127891853+ViktoriiaMatviichuk@users.noreply.github.com>
2023-08-17 12:20:49 +0300

    hw17

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import {Contacts} from './components/Contacts.jsx';
+
+
+
+
+function App() {
+  return (
+ 
+      <Contacts contacts={Contacts.contacts}/>
+
+     
+  )
+}
+
+export default App;
+
+
+
+
